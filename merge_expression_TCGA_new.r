bb4db2b072d90c8bb0fcadde9ab4b5fc732b8b55
smandape2015 <smandape@umail.iu.edu>
2016-12-07 14:31:32 -0600

    Final working code

--- merge_expression_TCGA_new.r
+++ merge_expression_TCGA_new.r
@@ -1,25 +1,32 @@
-setwd("E://smandape//Small Projects//TCGA//New folder//")
+setwd("E:/Gladus/Breast Cancer/")
 
+untar('AA_RNASeq_FPKM_gdc_download_20161207_172800.tar.gz')
 
-files<-read.csv("E://smandape//Small Projects//TCGA//New folder//gdc_manifest_20161014_163716.txt",header=T,sep="\t")
+files<-read.csv("E:/Gladus/Breast Cancer/MANIFEST.txt",header=T,sep="\t")
 expr<-data.frame(0)
 
 
 for(i in 1:nrow(files)){
-folder <- as.character(files[i,1])
-#print(folder)
-file_name <- as.character(files[i,2])
-#print(file_name)
+#  use the following if folder name and file name are in 
+#  seaprate columns
+#  folder <- as.character(files[i,1])
+#   #print(folder)
+#  file_name <- as.character(files[i,2])
+#   #print(file_name)
 
-tag<- paste(folder,file_name,sep="\\")
-x<-read.csv(tag,header=T,sep="\t",skip=1)
-print(x[1:2,])
+  file_name <- as.character(files[i,2])
+# tag<- paste(folder,file_name,sep="\\")
+# use tag instead of file_name if concatnating folder and file name
+# in read.csv and skip=1 for microarray exp data.
+  x<-read.csv(file_name,header=F,sep="\t")
+# print(x[1:2,])
 
-a<-as.vector(unlist(x[,2]))
-expr<-cbind(expr,a)
-gene<-x[,1]
-}
+  a<-as.vector(unlist(x[,2]))
+  expr<-cbind(expr,a)
+# gene<-x[,1]
+  }
 
-final_expr<-data.frame(genes=data.frame(gene),expr)
+final_expr<-data.frame(genes=x[,1],expr)
 
 
+write.table(final_expr,"E:/Gladus/Breast Cancer/final_exprexpr.txt",col.names=T,row.names=F,sep="\t")
