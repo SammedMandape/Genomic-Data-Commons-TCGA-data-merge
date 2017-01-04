2809cb65baeeb901f81b0c40dbd69f8183b234fd
smandape2015 <smandape@umail.iu.edu>
2017-01-03 18:14:20 -0600

    Combine pancreatic data and write with file name as column name.

--- merge_expression_TCGA_new.r
+++ merge_expression_TCGA_new.r
@@ -1,11 +1,11 @@
-setwd("E:/Gladus/Breast Cancer/")
+setwd("E:/Gladus/Pancreatic Cancer/African American")
 
-untar('AA_RNASeq_FPKM_gdc_download_20161207_172800.tar.gz')
+untar('AA_RNASeq_FPKM_gdc_download_20170103_215648.tar.gz')
 
-files<-read.csv("E:/Gladus/Breast Cancer/MANIFEST.txt",header=T,sep="\t")
+files<-read.csv("E:/Gladus/Pancreatic Cancer/African American/MANIFEST.txt",header=T,sep="\t")
 expr<-data.frame(0)
-
-
+colname <- vector()
+expr<-expr[,-1]
 for(i in 1:nrow(files)){
 #  use the following if folder name and file name are in 
 #  seaprate columns
@@ -20,13 +20,18 @@ for(i in 1:nrow(files)){
 # in read.csv and skip=1 for microarray exp data.
   x<-read.csv(file_name,header=F,sep="\t")
 # print(x[1:2,])
-
-  a<-as.vector(unlist(x[,2]))
+  header <- as.character(files[i,1])
+  colname <- c(colname, header)
+  
+  a <- as.vector(unlist(x[,2]))
   expr<-cbind(expr,a)
 # gene<-x[,1]
   }
 
-final_expr<-data.frame(genes=x[,1],expr)
+
+colnames(expr)<-colname
+#genes <- x[,1]
+final_expr<-data.frame(genes = x[,1],expr)
 
 
-write.table(final_expr,"E:/Gladus/Breast Cancer/final_exprexpr.txt",col.names=T,row.names=F,sep="\t")
+write.table(final_expr,"E:/Gladus/Pancreatic Cancer/African American/AA_pancreatic_final_expr.txt",col.names=T,row.names=F,sep="\t")
